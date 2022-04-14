845586c59641a176feeaf16596d1db4d3a646a3d
Hardwaregore <hardwaregore101@gmail.com>
2022-04-13 19:08:34 -0500

    THIS BETTER WORK

--- BASHTE.sh
+++ BASHTE.sh
@@ -6,13 +6,14 @@ echo "#  :wq = Write and quit  :q! = quit and discard           #"
 echo "###########################################################"
 echo ""
 read -p "Enter file name: " file
+touch $file
 clear
 echo "###########################################################"
-echo "#  BASHTE TEXT EDITOR - i = insert  :q = quit  :w = write #
+echo "#  BASHTE TEXT EDITOR - i = insert  :q = quit  :w = write #"
 echo "#  :wq = Write and quit  :q! = quit and discard           #"
 echo "###########################################################"
 read -p ">" store
-read -p ">" store
+awk 'NR==3{print $store}1' $file
 read -p ">" store
 read -p ">" store
 read -p ">" store
