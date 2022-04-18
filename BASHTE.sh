b2d55e3c71a10e4f965ed4ecf6697d46ba5fd891
Hardwaregore <hardwaregore101@gmail.com>
2022-04-18 15:12:01 -0500

    Update BASHTE.sh

--- BASHTE.sh
+++ BASHTE.sh
@@ -1,48 +1,53 @@
 #!/bin/bash
+# Ripoff vim
 clear
-echo "################################################################"
-echo "#  BASHTE TEXT EDITOR -  \\\ = interupt  :q = quit  :w = write. #"
-echo "#  :wq = Write and quit  :q! = quit and discard                #"
-echo "################################################################"
+echo "###############################################################################"
+echo "#  BASHTE TEXT EDITOR -  \\\ = interupt  :q = quit  :w = write                 #"
+echo "#  :wq = Write and quit  :q! = quit and discard  :dd = Delete Previous line   #"
+echo "###############################################################################"
 echo ""
 read -p "Enter file name: " file
-touch $file
-touch tempfile.txt
+touch .$file
+mkdir BASHTE
 clear
-echo "################################################################"
-echo "#  BASHTE TEXT EDITOR -  \\\ = interupt  :q = quit  :w = write. #"
-echo "#  :wq = Write and quit  :q! = quit and discard                #"
-echo "################################################################"
-
+echo "###############################################################################"
+echo "#  BASHTE TEXT EDITOR -  \\\ = interupt  :q = quit  :w = write                 #"
+echo "#  :wq = Write and quit  :q! = quit and discard  :dd = Delete Previous line   #"
+echo "###############################################################################"
 
 
 
 while true
-do  lines=$(wc -l tempfile.txt | cut -d " " -f 1)
+do  
     read -p "$lines >" store
     if [ "$store" = "\\:q" ]
     then
         break
     elif [ "$store" = "\\:w" ]
     then
-        cat tempfile.txt >> $file
+        ls BASHTE/* | sort -n | xargs cat >> $file
     elif [ "$store" = "\\:wq" ]
     then
-        cat tempfile.txt >> $file
-        rm -rf tempfile.txt
+        ls BASHTE/* | sort -n | xargs cat >> $file
+        rm -rf .$file
         break
     elif [ "$store" = "\\:q!" ]
     then
-        rm -rf tempfile.txt
+        rm -rf BASHTE
         rm -rf $file
         break
+
+    elif [ "$store" = "\\:dd" ]
+    then
+    LinesMinusOne=$(expr $lines - 1)
+    rm -rf BASHTE/$LinesMinusOne.txt
     else
-        echo $store >> tempfile.txt
+
+        echo $store >> BASHTE/$lines.txt
+        # counts the number of times the while loop is run
+        ((lines++))
     fi
 done
-
-
-
 
 
 
