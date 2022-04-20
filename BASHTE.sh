aa49db0764f17f2b3aef83064133cee6ddc3e427
TechnoDotΩ <73961295+TechDudie@users.noreply.github.com>
2022-04-19 21:44:38 -0500

    be a pro

--- BASHTE.sh
+++ BASHTE.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-# Ripoff vim
 clear
 echo "###############################################################################"
-echo "#  BASHTE TEXT EDITOR -  \\\ = interupt  :q = quit  :w = write                 #"
-echo "#  :wq = Write and quit  :q! = quit and discard  :dd = Delete Previous line   #"
+echo "#  BASHTE TEXT EDITOR -  \\\ = interupt  :q = quit  :w = write            your#"
+echo "#  :wq = Write and quit  :q! = quit and discard  :dd = Delete Previous linemom#"
 echo "###############################################################################"
 echo ""
 read -p "Enter file name: " file
 touch $file
-mkdir ~/BASHTE
+mkdir ~/.bashte
 clear
 echo "###############################################################################"
-echo "#  BASHTE TEXT EDITOR -  \\\ = interupt  :q = quit  :w = write                 #"
-echo "#  :wq = Write and quit  :q! = quit and discard  :dd = Delete Previous line   #"
+echo "#  BASHTE TEXT EDITOR -  \\\ = interupt  :q = quit  :w = write             dot#"
+echo "#  :wq = Write and quit  :q! = quit and discard  :dd = Delete Previous linecom#"
 echo "###############################################################################"
-
-
-
 while true
 do  
     read -p "$lines >" store
@@ -25,10 +21,10 @@ do
         break
     elif [ "$store" = "\\:w" ]
     then
-        ls ~/BASHTE/* | sort -n | xargs cat >> $file
+        ls ~/.bashte/* | sort -n | xargs cat >> $file
     elif [ "$store" = "\\:wq" ]
     then
-        ls ~/BASHTE/* | sort -n | xargs cat >> $file
+        ls ~/.bashte/* | sort -n | xargs cat >> $file
         rm -rf .$file
         break
     elif [ "$store" = "\\:q!" ]
@@ -36,15 +32,12 @@ do
         rm -rf BASHTE
         rm -rf $file
         break
-
     elif [ "$store" = "\\:dd" ]
     then
-    LinesMinusOne=$(expr $lines - 1)
-    rm -rf ~/BASHTE/$LinesMinusOne.txt
+    LMaO=$(expr $lines - 1)
+    rm -rf ~/.bashte/$LMaO.txt
     else
-
         echo $store >> ~/BASHTE/$lines.txt
-        # counts the number of times the while loop is run
         ((lines++))
     fi
 done
